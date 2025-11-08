5f91054c392d49fc033d9095aae5bf6acdcc5bd8
João Pedro Foscarini <jfoscarini@gmail.com>
2025-11-08 09:02:59 +0100

    update colorschemes

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -3,7 +3,16 @@ return {
         "EdenEast/nightfox.nvim",
         priority = 1000,
         config = function()
+            -- vim.cmd.colorscheme("nightfox")
             vim.cmd.colorscheme("dayfox")
+        end,
+    },
+    {
+        "jnz/studio98",
+        priority = 1000,
+        config = function()
+            -- vim.o.background = "light"
+            -- vim.cmd.colorscheme("studio98")
         end,
     },
 }
