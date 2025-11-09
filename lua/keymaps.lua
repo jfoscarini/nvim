13d480adac13a0f349ea9bbc1ddbc5395715883e
João Pedro Foscarini <jfoscarini@gmail.com>
2025-11-09 13:35:24 +0100

    better buffer quitting

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -5,4 +5,12 @@ vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right win
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
-vim.keymap.set('n', '<D-w>', "<cmd>bd<CR>", { noremap = true, silent = true, desc = "Close current buffer and go to the next" })
+vim.keymap.set("n", "<D-w>", function()
+    local bufnr = vim.api.nvim_get_current_buf()
+    if vim.bo[bufnr].buftype == "" then
+        vim.cmd("bnext")
+        vim.cmd("bdelete " .. bufnr)
+    else
+        vim.cmd("close")
+    end
+end, { desc = "Close the current buffer" })
--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -3,12 +3,15 @@ return {
     event = "VimEnter",
     opts = {
         spec = {
+            { "<leader><leader>", hidden = true },
             { "<leader>/", group = "[/] Search"},
         },
     },
     config = function(_, opts)
         local wk = require("which-key")
         wk.setup(opts)
-        vim.keymap.set("n", "<leader>", "<cmd>WhichKey<CR>", { desc = "<leader>" })
+        vim.keymap.set("n", "<leader><leader>", function()
+            wk.show("<leader>", { mode = "n" })
+        end, { silent = true })
     end,
 }
