fdd815ed31e97df20caf47bb3ee4111da44f2007
João Pedro Foscarini <jfoscarini@gmail.com>
2025-11-10 09:05:04 +0100

    add search all to telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -26,18 +26,19 @@ return {
         pcall(require("telescope").load_extension, "ui-select")
 
         local builtin = require("telescope.builtin")
-        vim.keymap.set("n", "<leader>/h", builtin.help_tags, { desc = "Search [H]elp" })
-        vim.keymap.set("n", "<leader>/k", builtin.keymaps, { desc = "Search [K]eymaps" })
-        vim.keymap.set("n", "<leader>/f", builtin.find_files, { desc = "Search [F]iles" })
-        vim.keymap.set("n", "<leader>/d", builtin.diagnostics, { desc = "Search [D]iagnostics" })
+        vim.keymap.set("n", "<leader>/a", builtin.live_grep, { desc = "Search in [A]ll Project" })
         vim.keymap.set("n", "<leader>/b", builtin.buffers, { desc = "Search in Existing [B]uffers" })
-        vim.keymap.set("n", "<leader>/.", builtin.oldfiles, { desc = "Search Recent Files [. repeat]" })
+        vim.keymap.set("n", "<leader>/d", builtin.diagnostics, { desc = "Search [D]iagnostics" })
+        vim.keymap.set("n", "<leader>/f", builtin.find_files, { desc = "Search [F]iles" })
         vim.keymap.set("n", "<leader>/g", function ()
             builtin.live_grep({
                 grep_open_files = true,
                 prompt_title = "Live Grep in Open Files",
             })
         end, { desc = "Search by [G]rep" })
+        vim.keymap.set("n", "<leader>/h", builtin.help_tags, { desc = "Search [H]elp" })
+        vim.keymap.set("n", "<leader>/k", builtin.keymaps, { desc = "Search [K]eymaps" })
+        vim.keymap.set("n", "<leader>/.", builtin.oldfiles, { desc = "Search Recent Files [. repeat]" })
         vim.keymap.set("n", "<leader>//", function()
             builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
                 winblend = 10,
