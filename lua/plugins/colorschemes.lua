13003cfc8ed5de44887d5ea6a7b4250a49359ce6
João Foscarini <jfoscarini@users.noreply.github.com>
2025-11-25 17:56:29 +0100

    Reorganize colorscheme configuration for plugins, nightfox as default

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,21 +1,21 @@
 return {
-    {
-        "EdenEast/nightfox.nvim",
-        priority = 1000,
-        config = function()
-            -- vim.cmd.colorscheme("nightfox")
-            vim.cmd.colorscheme("dayfox")
-        end,
-    },
-    {
-        "jnz/studio98",
-        priority = 1000,
-        config = function()
-            -- vim.o.background = "light"
-            -- vim.cmd.colorscheme("studio98")
-        end,
-    },
-    {
+	{
+		"EdenEast/nightfox.nvim",
+		priority = 1000,
+		config = function()
+			vim.cmd.colorscheme("nightfox")
+			-- vim.cmd.colorscheme("dayfox")
+		end,
+	},
+	{
+		"jnz/studio98",
+		priority = 1000,
+		config = function()
+			-- vim.o.background = "light"
+			-- vim.cmd.colorscheme("studio98")
+		end,
+	},
+	{
 		"ribru17/bamboo.nvim",
 		priority = 1000,
 		config = function()
