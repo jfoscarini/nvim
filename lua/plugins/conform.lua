edfca1e5483ebbde5ead84ade1c3fed0efedabf5
João Pedro Foscarini <jfoscarini@gmail.com>
2025-11-27 10:36:01 +0100

    reorder conform shortcuts

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -2,16 +2,6 @@ return {
 	"stevearc/conform.nvim",
 	event = { "BufWritePre" },
 	cmd = { "ConformInfo" },
-	keys = {
-		{
-			"<leader>f",
-			function()
-				require("conform").format({ async = true, lsp_format = "fallback" })
-			end,
-			mode = "",
-			desc = "[F]ormat buffer",
-		},
-	},
 	opts = {
 		notify_on_error = false,
 		formatters_by_ft = {
@@ -21,6 +11,15 @@ return {
 			lua = { "stylua" },
 			python = { "isort", "black" },
 			typescript = { "prettierd", "prettier", stop_after_first = true },
+		},
+	},
+	keys = {
+		{
+			"<leader>f",
+			function()
+				require("conform").format({ async = true, lsp_format = "fallback" })
+			end,
+			desc = "[F]ormat buffer",
 		},
 	},
 }
